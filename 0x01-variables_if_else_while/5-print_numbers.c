38995b89da7897eafaffae9804d9342856f27681
Arnold Otieno <arnoldotieno31@gmail.com>
2023-08-12 07:09:27 -0700

    printing o to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Prints numbers from zero to 9
+ * Return: returns zero for success
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+	printf("%d", i);
+	}
+	i++;
+	return (0);
+}
