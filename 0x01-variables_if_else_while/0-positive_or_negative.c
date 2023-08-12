54253cdc415424dcc4c43590aa13260c65ccfb4d
Arnold Otieno <arnoldotieno31@gmail.com>
2023-08-12 01:17:34 -0700

    Positive or negative check

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - Checks if a number is positive or negative
+ *
+ * Return: zero for success
+*/
 int main(void)
 {
 	int n;
