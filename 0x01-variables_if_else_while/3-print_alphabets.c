d1c39748850c33e547d9f17deaef7a63222b1608
Arnold Otieno <arnoldotieno31@gmail.com>
2023-08-12 05:16:27 -0700

    Printing lowercae and uppercase alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - The program prints alphabets
+ * in lowercase and uppercase
+ * Return: return zero on success
+*/
+int main(void)
+{
+	char myChar = 'a';
+
+	while (myChar <= 'z')
+	{
+	putchar(myChar);
+	myChar++;
+	}
+	myChar = 'Z';
+
+	while (myChar <= 'Z')
+	{
+	putchar(myChar);
+	myChar++;
+	}
+	putchar('\n');
+	return (0);
+}
