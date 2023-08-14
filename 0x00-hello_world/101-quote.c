cf05247bb6ff2e3f7d6ec70073c1f0f9f36d0c10
Arnold Otieno <arnoldotieno31@gmail.com>
2023-08-14 04:05:45 -0700

    Printing to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - prints a line to standard error
- * Return: returns one
+ * main - Entry point of the program
+ *
+ * Description: Prints a line to standard output
+ * Return: Always returns 1
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar,
-		       	2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Kor
+par, 2015-10-19\n", stdout);
 	return (1);
 }
+
