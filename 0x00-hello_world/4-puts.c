6df7eff27268d18197ef8b30c1e61eb4e3c27762
Arnold Otieno <arnoldotieno31@gmail.com>
2023-08-14 01:42:30 -0700

    Printing using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Program prints words
+ * Return: zero for success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
