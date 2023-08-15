583e04af4a405292ac3e3fddd90b937811cb0ecd
Arnold Otieno <arnoldotieno31@gmail.com>
2023-08-15 12:03:46 -0700

    Printing lowercase letters ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabets ten times
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		char alpbt;
+
+		for (alpbt = 'a'; alpbt <= 'z'; alpbt++)
+		{
+			putchar(alpbt);
+		}
+		i++;
+		putchar('\n');
+	}
+}
