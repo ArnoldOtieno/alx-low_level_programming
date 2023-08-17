3f8274bb36fe5aa1af52344deef0432d996aa602
Arnold Otieno <arnoldotieno31@gmail.com>
2023-08-17 13:56:13 -0700

    Printing zero to nine without 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints 0 to 9 except 2 and 4
+ * Return: returns void
+ */
+void print_most_numbers(void)
+{
+	char y;
+
+	for (y = '0'; y <= '9'; y++)
+	{
+		if (!(y == '2' || y == 4))
+			_putchar(y);
+	}
+	_putchar('\n');
+}
